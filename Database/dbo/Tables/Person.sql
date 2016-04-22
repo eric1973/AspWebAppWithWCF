9275d99f43444d1fe4950e640474fda2b1a011e2
erich brunner <erich.brunner@live.at>
2016-04-22 10:43:52 +0200

    added Database

---
+++ Database/dbo/Tables/Person.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Person] (
+    [id]      INT           NOT NULL,
+    [name]    VARCHAR (MAX) NULL,
+    [address] VARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_Person] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
---
+++ Database/dbo/Tables/PersonOrders.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[PersonOrders] (
+    [id]          INT           IDENTITY (1, 1) NOT NULL,
+    [personId]    INT           NOT NULL,
+    [description] VARCHAR (MAX) NOT NULL,
+    [amount]      INT           NOT NULL,
+    [notes]       VARCHAR (MAX) NULL,
+    CONSTRAINT [PK_PersonOrders] PRIMARY KEY CLUSTERED ([id] ASC),
+    CONSTRAINT [FK_PersonOrders_Person] FOREIGN KEY ([personId]) REFERENCES [dbo].[Person] ([id])
+);
+
